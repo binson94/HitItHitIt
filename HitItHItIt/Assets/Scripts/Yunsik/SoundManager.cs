3dce90688871e31551c4fa1ab3e34d34b2ae8223
rilato <87744890+rilato@users.noreply.github.com>
2022-05-20 23:57:43 +0900

    Modify_Main_Scene

--- HitItHItIt/Assets/Scripts/Yunsik/SoundManager.cs
+++ HitItHItIt/Assets/Scripts/Yunsik/SoundManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
     public AudioSource musicsource;
+
     public void SetMusicVolume(float volume)
     {
         musicsource.volume = volume;
     }
+
+    // Playerprefs로 데이터를 저장
+    // 볼륨 사이즈를 저장하여 다른 씬에도 적용되도록 설정
 }
