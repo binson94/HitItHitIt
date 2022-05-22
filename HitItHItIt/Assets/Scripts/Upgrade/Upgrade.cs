47f3fa248b708231c542e41d4660fbfcaba15e5c
22hyunmin <yhm9747@naver.com>
2022-05-23 06:01:59 +0900

    (temporary)Upgrade
    .

--- HitItHItIt/Assets/Scripts/Upgrade/Upgrade.cs
+++ HitItHItIt/Assets/Scripts/Upgrade/Upgrade.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;
 
 public class Upgrade : MonoBehaviour
 {
-    int upgrade_Cost = 100;
-    enum attack
-    {
-         RUpper, RJap, RHook, LUpper, LJap, LHook 
-    }
+    const int RUpper = 0;
+    const int RJap = 1; 
+    const int RHook = 2;
+    const int LUpper = 3;
+    const int LJap = 4;
+    const int LHook = 5;
+    const int stamina = 6;
+
+    int initialCost = 100;    
+
 
 //0,1,2 = RUpper, RJap, RHook, 3,4,5 = LUpperm LJap, LHook로 설정
-    int[] powerLevel = {1, 1, 1, 1, 1, 1};
+    int[] powerLevel = {1, 1, 1, 1, 1, 1,1};
 
 
     public int level_Up(int a)
@@ -22,19 +27,42 @@ public class Upgrade : MonoBehaviour
     }
 //아직 강해지는 정도는 설정하지 않아서 단순히 1을 증가시키는 것으로 설정
 //int a = 기존의 파워, int b = 현재 레벨 수준
-    public int power(int a, int b)
+    public int power(int a)
     {
-        int c = a + 1*(b);
-        return c;
+        int b = 100*(a);
+        return b;
     }
 //int a = 기존의 가격, int b = 현재 레벨 수준
     public int cost(int a, int b)
     {
-        int c = a + a*(b-1);
+        int c = a*b ;
         return c;
     }
 
+    public void Game(int whatToUpgrade)
+    {
 
+        int costNeeded = cost(initialCost,powerLevel[whatToUpgrade]);
+        int money = GameObject.Find("MoneyMge").GetComponent<TEMP_Money>().moneyToken;
+
+        if ( money - costNeeded  >= 0)
+        {
+            powerLevel[whatToUpgrade] += 1; 
+            money -= costNeeded;
+            Debug.Log("강화 성공, 남은 돈은 다음과 같습니다.:");
+            Debug.Log(money);
+            Debug.Log("현재 강화레벨:");               
+            Debug.Log(powerLevel[whatToUpgrade]); 
+            
+        }
+
+        else
+        {
+            Debug.Log("돈이 부족합니다");
+        }
+
+    }
+ 
 }
 
 
