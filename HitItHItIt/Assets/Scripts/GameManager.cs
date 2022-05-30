9d3695a0caf77a619176d17933ab0ea171e60044
binson94 <dnduf33@gmail.com>
2022-05-30 22:12:42 +0900

    edit gitignore

--- HitItHItIt/Assets/Scripts/GameManager.cs
+++ HitItHItIt/Assets/Scripts/GameManager.cs
@@ -16,14 +16,41 @@ public class GameManager : MonoBehaviour
                 container.name = "GameManager";
                 _instance = container.AddComponent(typeof(GameManager)) as GameManager;
                 DontDestroyOnLoad(container);
+
+                LoadData();
             }
 
             return _instance;
         }
     }
 
-    public void LoadData()
+    static GameData _gamedata = new GameData();
+    public GameData gameData
     {
-        Debug.Log("데이터 로드");
+        get
+        {
+            return _gamedata;
+        }
     }
+
+    static void LoadData()
+    {
+        _gamedata.money = PlayerPrefs.GetInt("Money", 0);
+        for(int i = 0;i < 4;i++) _gamedata.powLvls[i] = PlayerPrefs.GetInt($"PowLvl{i}");
+        _gamedata.stage = PlayerPrefs.GetInt("Stage", 1);
+    }
+
+    void SaveData()
+    {
+        PlayerPrefs.SetInt("Money", _gamedata.money);
+        for(int i = 0;i < 4;i++) PlayerPrefs.SetInt($"PowLvl{i}", _gamedata.powLvls[i]);
+        PlayerPrefs.SetInt("Stage", _gamedata.stage);
+    }
+}
+
+public class GameData
+{
+    public int money;
+    public int[] powLvls = new int[4];
+    public int stage;
 }
--- HitItHItIt/Assets/Scripts/Title/TitleManager.cs
+++ HitItHItIt/Assets/Scripts/Title/TitleManager.cs
@@ -15,7 +15,6 @@ namespace Yeol
         private void Start()
         {
             LoadSoundOption();
-            GameManager.instance.LoadData();
             SoundMgr.instance.PlayBGM(0);
         }
 
