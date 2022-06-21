74585d9d625866a72e9d78e44021a4b845960d3a
groot_ljm <beckaljm@naver.com>
2022-06-21 16:09:58 +0900

    랜덤 출력

--- HitItHItIt/Assets/Scripts/Money/MoneyManager.cs
+++ HitItHItIt/Assets/Scripts/Money/MoneyManager.cs
@@ -71,6 +71,8 @@ namespace Yeol
             StartCoroutine(WaitBeforeStart());
         }
 
+
+
         IEnumerator WaitBeforeStart()
         {
             int time = 3;
@@ -86,7 +88,32 @@ namespace Yeol
             startTxt.gameObject.SetActive(false);
             StartAttack();
         }
-
+        //랜덤 enemy 이미지 생성 (나무, 돌, 광석)
+        
+        public GameObject Enemys; 
+        public Image DrawImage; // 랜덤 이미지를 출력할 오브젝트
+        public Sprite Image1;
+        public Sprite Image2;
+        public Sprite Image3;
+        public int RandomInt; // 랜덤 변수
+        void Update()
+        {
+            RandomInt = Random.Range(0, 3);
+        }
+        public void choose()
+        {
+            Enemys.SetActive(false);
+            if (RandomInt == 1) DrawImage.sprite = Image1;
+            else if (RandomInt == 2) DrawImage.sprite = Image2;
+            else if (RandomInt == 3) DrawImage.sprite = Image3;
+            invoke("CloseImage", 1.0f);
+        }
+        public void CloseImage()
+        {
+            DrawImage.sprite = null;
+            Enemys.SetActive(true);
+        }
+        //-----------------------------------------------------------
         ///<summary> 디버그 용, 키보드 입력과 버튼 대응 </summary>
         private void Update()
         {
